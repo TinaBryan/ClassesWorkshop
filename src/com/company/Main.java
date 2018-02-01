4325ea2710ae94b94601096b0224e9d296cdf020
TinaBryan <tdmak63@gmail.com>
2018-02-01 15:17:02 -0500

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    private String MyString;
+
+    public static void main(String[] args) {
+	// write your code here
+
+        // A class should do one thing
+        // should handle one thing
+
+
+    }
+
+}
